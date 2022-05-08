58f330180b9ab7ef7df2565912a3490d6b872b1b
git@github.com:adrianapvent/git-it-done.git <adriana.e.pena@gmail.com>
2022-05-08 16:52:27 -0500

    test folders created

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('Hello Node!');
+// const inquirer = require("inquirer");
+// const fs = require("fs");
+// const Employee = require("./__tests__/Employee.test");
+// const Engineer = require("./__tests__/Engineer.test.js");
+// const Intern = require("./__tests__/Intern.test.js");
+// const Manager = require("./__tests__/Manager.test");
+
+// const employees = [];
+
+// function initApp() {
+//     startHtml();
+//     addMember();
+// }
