3e0c7fd7d211e0fb6838e844afb9635ce88fa1f6
git@github.com:adrianapvent/git-it-done.git <adriana.e.pena@gmail.com>
2022-05-08 18:26:05 -0500

    folders

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('Hello Node!');
 // const inquirer = require("inquirer");
-// const fs = require("fs");
+const fs = require("fs");
 // const Employee = require("./__tests__/Employee.test");
 // const Engineer = require("./__tests__/Engineer.test.js");
 // const Intern = require("./__tests__/Intern.test.js");
