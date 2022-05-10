b8ffc19358fe98a973472051fa9705c84e2d8826
git@github.com:adrianapvent/git-it-done.git <adriana.e.pena@gmail.com>
2022-05-10 00:12:46 -0500

    intern testing progress, 1 error, mvp

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,16 +2,22 @@ const Engineer = require('../lib/assets/js/Engineer.js');
 
 // create Engineer object
 test('create Engineer object', () => {
-    const engineer = new Engineer ('name', 'id', 'email', 'github');
+    const engineer = new Engineer('name', 'id', 'email', 'github');
 
     expect(engineer.github).toBe('github');
 });
 
 // get Engineer github
 test('get Engineer github', () => {
-    const engineer = new Engineer ('name', 'id', 'email', 'github');
+    const engineer = new Engineer('name', 'id', 'email', 'github');
 
     expect(engineer.getGithub()).toEqual(expect.any(String));
 });
 
 // get Engineer role
+test('gets role of employee', () => {
+    const engineer = new Engineer('name', 'id', 'email', 'github');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
+
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,10 +1,21 @@
-const { test } = require('picomatch');
 const Intern = require('../lib/assets/js/Intern.js');
 
 test('create Intern object', () => {
+    const intern = new Intern('name', 'id', 'email', 'school');
 
+    expect(intern.school).toBe('school');
 });
 
 // get Intern school
+test('get intern school name', () => {
+    const intern = new Intern('name', 'id', 'email', 'school');
+
+    expect(intern.getSchool()).toBe(intern.school);
+});
 
 // get Intern role
+test('get intern role', () => {
+    const intern = new Intern('name', 'id', 'email', 'school');
+
+    expect(intern.getRole()).toEqual("Intern");
+});
